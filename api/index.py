c95b30c89fe0d2d1548b8821c733378d471bdcbe
tn9cxt46s7-gif <tn9cxt46s7@privaterelay.appleid.com>
2026-07-19 23:31:39 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Request
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from dotenv import load_dotenv
 import os
 import logging
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 load_dotenv()
 
@@ -13,6 +16,12 @@ bot = Bot(
     default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
 dp = Dispatcher()
+
+from handlers.user import router as user_router
+from handlers.admin import router as admin_router
+
+dp.include_router(user_router)
+dp.include_router(admin_router)
 
 app = FastAPI()
 
@@ -26,7 +35,3 @@ async def webhook(request: Request):
 @app.get("/")
 async def root():
     return {"status": "bot is running"}
-
-# Запуск handlers (добавь свои)
-# from bot.handlers.user import router as user_router
-# dp.include_router(user_router)
